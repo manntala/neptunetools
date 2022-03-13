fa967abca65de7302bc2f9c157d2f099e4281f7d
MannyGT Talaroc <magnezyle@gmail.com>
2022-03-13 21:53:11 +0800

    installed psycopg2

--- neptune/settings.py
+++ neptune/settings.py
@@ -86,13 +86,36 @@ WSGI_APPLICATION = 'neptune.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'daeh43s6gh3viv',
+#             'USER': 'tvrhrbqgnohhah',
+#             'PASSWORD': '96c563a13d816f86f7a74f9cebd2966f3ff66cb4237457eb9d2f269e8f7c3efb',
+#             'HOST': 'ec2-44-195-191-252.compute-1.amazonaws.com',
+#             'PORT': '5432'
+#         }
+#     }
 
-DATABASES = {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
